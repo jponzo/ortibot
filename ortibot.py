2d83f020158564167893f29203d90c1b2495452b
Federico Falour <ffalour@ararffalour.ml.com>
2016-09-23 12:41:34 -0300

    trolli bulling fix v2

--- ortibot.py
+++ ortibot.py
@@ -82,7 +82,7 @@ def any_message(bot, update):
         resp_string = "Cerra el orto, Cusa. Sos de Platense" 
     elif "trolli" in message.lower() or "agustin" in sender_name.lower():
         lista=[1,2,3,4,5]
-        if random.lista == 3:
+        if random.choice(lista) == 3:
             resp_string = "Trolli este es tuyo??"
             bora_images=['http://img.ar.autos.cozot.com/pics/ar/2015/08/22/Vw-bora-chocado-el-la-trompa-General-Roca-20150822215257.jpg', 'http://imganuncios.mitula.net/vendo_volkswagen_bora_1_8t_full_con_cuero_2011_5520130462999686988.jpg', 'http://i.ebayimg.com/00/s/NjAwWDgwMA==/z/9doAAOSw3mpXOJqo/$_20.jpg', 'http://www.diariohuarpe.com/wp-content/uploads/2014/05/Pol-AutoChocadoBoraHilux-27052014-640x375.jpg', 'http://www.noticiaspv.com/wp-content/uploads/2012/04/20120407_valle_2593.jpg']
             image=random.choice(bora_images)
